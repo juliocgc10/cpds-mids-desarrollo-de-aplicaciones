5117641846d8e52e1fd128e6f5068093168cc497
Julio César Guzmán Cortés <juliocgc10@hotmail.com>
2021-07-20 02:09:38 -0500

    Modulo 1 carga en GitHub

---
+++ MIDS-DES_APP/Modulo1_ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo1_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejemplo de código C# en GitHub...");
+            Console.ReadKey();
+        }
+    }
+}
